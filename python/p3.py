492c7ef80956eaef132a324bd3de464e1f3daa02
Anson MacKeracher <anson@tophatmonocle.com>
2012-07-28 01:18:01 -0400

    Adds problem 3 and nodes (INCOMPLETE)

---
+++ python/p3.py
@@ -0,0 +1,7 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+# TODO
+# Maybe use a seive to generate primes?
+# Iterate over primes to find largetst factor
