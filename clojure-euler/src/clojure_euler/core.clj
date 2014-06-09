dc48293505ba3e8de00c07f04c911bd4c1fe087e
Anson MacKeracher <amackera@gmail.com>
2014-06-09 13:05:59 -0400

    Adds implementation of problem 1

--- clojure-euler/src/clojure_euler/core.clj
+++ clojure-euler/src/clojure_euler/core.clj
@@ -1,9 +1,21 @@
 (ns clojure-euler.core
   (:gen-class))
 
+(defn doit [sum, value]
+  (if (or
+        (= (mod value 3) 0)
+        (= (mod value 5) 0))
+    (+ sum value)
+    sum))
+
+;; Find the sum of all the multiples of 3 or 5 below 1000
+(defn p1 []
+  (reduce doit (range 1000)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (p1))
+
