17ee891ff35e46321bd37d112ac4233382117228
Anson MacKeracher <amackera@gmail.com>
2014-06-09 12:53:02 -0400

    Adds clojure project folder

---
+++ clojure-euler/project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-euler "0.1.0-SNAPSHOT"
+  :description "Project Euler problem solutiosn in Clojure"
+  :url "http://mackeracher.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main clojure-euler.core)
